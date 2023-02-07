1492191ece7c4626b5805f3ee6799381da381ef1
GShir97 <goldshir97@gmail.com>
2023-02-08 01:21:22 +0200

    .

--- create_dataset.py
+++ create_dataset.py
@@ -1,38 +1,5 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-#
-# # Initialize the webdriver
-# driver = webdriver.Chrome()
-#
-# # Navigate to the Fox News website
-# driver.get("https://www.foxnews.com/")
-#
-# # Wait until the articles are loaded
-# wait = WebDriverWait(driver, 10)
-# articles = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//section[@class='collection collection-article-list")))
-#
-# # //h1[@class='headline']
-# # //section[@class='collection collection-article-list']
-# # //section[@class='collection collection-article-list has-load-more']
-#
-#
-# # Extract the article name and category for each article
-# for article in articles:
-#     title_element = article.find_element_by_xpath(".//h2[@class='title']/a")
-#     title = title_element.text
-#     category_element = article.find_element_by_xpath(".//span[@class='primary-category']/a")
-#     category = category_element.text
-#     print("Title:", title)
-#     print("Category:", category)
-#
-# # Close the webdriver
-# driver.quit()
 
 from selenium import webdriver
-from selenium.common import StaleElementReferenceException
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -53,30 +20,6 @@ data = [
     #   title
     # }
 ]
-
-
-# def extract_articles_from_article_section(section):
-#     # Extract the article name and category for each article
-#     articles = section.find_elements(By.XPATH, ".//article")
-#     for article in articles:
-#         title_element = article.find_element(By.XPATH, ".//h4[@class='title']/a")
-#         title = title_element.text
-#         category_element = article.find_element(By.XPATH, ".//span[@class='eyebrow']/a")
-#         category_name = category_element.text
-#         # skip video articles
-#         if category_name == 'VIDEO':
-#             continue
-#         data.append({"title": title})
-#
-#
-# # first sections block
-# article_section = wait.until(
-#     EC.presence_of_element_located((By.XPATH, "//section[@class='collection collection-article-list']")))
-#
-# extract_articles_from_article_section(article_section)
-#
-# actions = ActionChains(driver)
-
 
 def extract_articles_from_article_section_with_load_more(section, number_of_articles=100):
     # Extract the article name and category for each article
